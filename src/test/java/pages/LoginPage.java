3a551d8aada791d270a8f1e2caecdc0b4254b465
Aleksandra <taeyeononni@gmail.com>
2022-01-04 21:24:33 +0300

    Second Add

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,38 +1,36 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import tests.BaseTest;
 
 public class LoginPage extends BasePage {
 
-    //WebDriver driver;
 
     public static final By USERNAME_INPUT = By.id("user-name");
     public static final By PASSWORD_INPUT = By.id("password");
     public static final By LOGIN_BUTTON = By.xpath("//input[@value='Login']");
-    public static final By ERROR_MASSAGE = By.cssSelector(".error-massage-container");
+    public static final String ERROR_MASSAGE = "//h3[@data-test='error']";
 
-    public LoginPage(WebDriver driver){
+    public LoginPage(WebDriver driver) {
         super(driver);
     }
-
-    public void open(){
+    @Step("Open website")
+    public void open() {
 
         driver.get("https://www.saucedemo.com/");
     }
-
-    public void login(String userName , String password){
+    @Step("Login as user name {******} use password {*******}")
+    public void login(String userName, String password) {
         driver.findElement(USERNAME_INPUT).sendKeys(userName);
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
         driver.findElement(LOGIN_BUTTON).click();
     }
+    @Step("Get error massage")
+    public String getErrorMassage() {
 
-    public String getErrorMassage(){
-
-        return driver.findElement(ERROR_MASSAGE).getText();
+        return driver.findElement(By.xpath(ERROR_MASSAGE)).getText();
     }
-
 
 
 }
--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -2,32 +2,61 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-                           // НИ ОДИН ТЕСТ НЕ СРАБАТЫВАЕТ, Я НЕ МОГУ ПОНЯТЬ ЧТО Я НЕ ТАК ДЕЛАЮ
-public class CheckoutTest  extends BaseTest{
-    @Test
-    public void checkoutIsDisplay(){
+
+
+public class CheckoutTest extends BaseTest {
+    @Test(description = "Checkout page is display ")
+    public void checkoutIsDisplay() {
         loginPage.open();
-        loginPage.login("standard_user","secret_sauce");
+        loginPage.login("standard_user", "secret_sauce");
 
         inventoryPage.cartClick();
         cartPage.checkoutButtonClick();
 
-        checkoutPage.checkOut("Alex","Petrov","246056");
-        checkoutPage.checkoutContinueButtonClick();
-        Assert.assertEquals(checkoutTwoPage.checkoutTwoButtonFinish(),"true","Вы не вошли на страницу");
+        checkoutPage.checkOut("Alex", "Petrov", "246056");
+        Assert.assertEquals(checkoutTwoPage.checkoutTwoButtonFinish(), true, "Вы не вошли на страницу");
 
-}
-    @Test
-    public void checkoutNotDisplayWithoutFirstName(){
+    }
+
+    @Test(description = "Check error massage without first name")
+    public void checkoutNotDisplayWithoutFirstName() {
         loginPage.open();
-        loginPage.login("standard_user","secret_sauce");
+        loginPage.login("standard_user", "secret_sauce");
 
         inventoryPage.cartClick();
         cartPage.checkoutButtonClick();
 
-        checkoutPage.checkOut("","Petrov","246056");
+        checkoutPage.checkOut("", "Petrov", "246056");
         checkoutPage.checkoutContinueButtonClick();
-        Assert.assertEquals(checkoutPage.getErrorMassageCheckout(),"Error: First Name is required","Сообщение об ощибке неверно");
+        Assert.assertEquals(checkoutPage.getErrorMassageCheckout(), "Error: First Name is required", "Сообщение об ощибке неверно");
 
+    }
+
+    @Test(description = "Check error massage without postal code")
+    public void checkoutNotDisplayWithoutCode() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+
+        inventoryPage.cartClick();
+        cartPage.checkoutButtonClick();
+
+        checkoutPage.checkOut("Alex", "Petrov", "");
+        checkoutPage.checkoutContinueButtonClick();
+        Assert.assertEquals(checkoutPage.getErrorMassageCheckout(), "Error: Postal Code is required", "Сообщение об ощибке неверно");
+
+    }
+
+    @Test(description = "Check there is final page")
+    public void checkoutNotDisplayFinlaPage() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+
+        inventoryPage.cartClick();
+        cartPage.checkoutButtonClick();
+
+        checkoutPage.checkOut("Alex", "Petrov", "123456");
+
+        checkoutTwoPage.buttonFinishClick();
+        Assert.assertEquals(checkoutTwoPage.setFinishH2(), "THANK YOU FOR YOUR ORDER", "Вы не на финальной странице");
     }
 }
